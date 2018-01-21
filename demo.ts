4fda10e65d6eb1b7de6009aef3c4ac5e12e15d52
XGHeaven <xgheaven@gmail.com>
2018-01-21 14:57:44 +0800

    init commit

---
+++ demo.ts
@@ -0,0 +1,17 @@
+import { extname } from 'path'
+
+const vaiable = 1
+
+interface Module {
+    name: string
+}
+
+class NodeModule {
+    constructor() {
+        return
+    }
+
+    version() {
+        return '0.0.0'
+    }
+}

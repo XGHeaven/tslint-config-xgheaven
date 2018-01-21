012a308c16d29a196bc79197f49a1ee9987af335
XGHeaven <xgheaven@gmail.com>
2018-01-21 15:21:23 +0800

    remove ban-types, add object key order

--- demo.ts
+++ demo.ts
@@ -6,6 +6,9 @@ interface Module {
     name: string
 }
 
+interface EmptyInterface {
+}
+
 class NodeModule {
     constructor() {
         return
@@ -14,4 +17,24 @@ class NodeModule {
     version() {
         return '0.0.0'
     }
+}
+
+const func: Function = () => {
+    // EMPTY
+}
+
+const objOrder = {
+    func,
+    A: () => void 0,
+    NodeModule: () => void 0,
+}
+
+const objOrder2: {
+    b: number,
+    a: number,
+    c: number,
+} = {
+    a: 2,
+    b: 1,
+    c: 3,
 }
